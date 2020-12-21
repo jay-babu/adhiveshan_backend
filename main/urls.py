2ac2b61cec6631f5ca43f0ff45e297bc310e8a5a
Darshan <darshan@Darshans-MacBook-Pro-2.local>
2020-12-21 13:53:28 -0500

    Remove api from urls

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,6 @@ from . import model_api
 
 urlpatterns = [
     path('users/', model_api.UsersEndpoint.as_view(), name='main/users'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
