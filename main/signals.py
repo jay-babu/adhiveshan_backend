c07a616c04e7da0a64089a5beff87713fff2aa45
Jay Patel <jay.r.patel2115@gmail.com>
2021-01-03 22:31:40 -0500

    Fixing Url for reset_password_url

--- main/signals.py
+++ main/signals.py
@@ -23,7 +23,7 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
         'current_user': reset_password_token.user,
         'username': reset_password_token.user.email,
         'email': reset_password_token.user.email,
-        'reset_password_url': "https://bkadhiveshan.na.baps.org/#/auth/password-reset/confirm{}?token={}".format(
+        'reset_password_url': "https://bkadhiveshan.na.baps.org/#/auth{}?token={}".format(
             reverse('password_reset:reset-password-confirm'), reset_password_token.key)
     }
 
