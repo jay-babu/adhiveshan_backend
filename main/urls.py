d296d30547e304cdbb9cd94881f2ce9a2293ae84
Darshan <darshanvkalola@gmail.com>
2021-01-16 20:06:46 -0500

    Remove dev paths

--- main/urls.py
+++ main/urls.py
@@ -30,6 +30,6 @@ urlpatterns = [
     path('reset_bookmarked_all/', views.ResetBookmarkedView.as_view(), name='reset_bookmarked_all'),
 
     # Dev use only
-    path('upload_content/', views.UploadContentView.as_view(), name='upload_content'),
-    path('add_modules/', views.AddModulesToDB.as_view(), name='add_modules'),
+    # path('upload_content/', views.UploadContentView.as_view(), name='upload_content'),
+    # path('add_modules/', views.AddModulesToDB.as_view(), name='add_modules'),
 ]
