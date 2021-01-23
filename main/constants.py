25006d8ba3d6d789443b884e77959d4640c6f4e7
Neel Patel <62853591+neelpateldev@users.noreply.github.com>
2021-01-22 23:49:46 -0600

    Update constants.py

--- main/constants.py
+++ main/constants.py
@@ -684,15 +684,11 @@ BAL_FAQ = {
         },
         {
             'question': 'Which Adhiveshan do I pledge for and which Summer Shibir do I go to?',
-            'answer': 'You will pledge for the tiers in the group you currently are in (<b>as of January 2021</b>). For example, if you are in Group 2 in January of 2021, and even if you are graduating and moving to Group 3 in September 2021, you will pledge for Adhiveshan in the Group 2 categories. Anyone in Groups 1-3 ( as of January 2021) will also attend the <b>Bal/Balika Summer Shibir</b>.',
+            'answer': 'You will pledge for the tiers in the group you currently are in (<b>as of January 2021</b>). For example, if you are in Group 2 in January of 2021, and even if you are graduating and moving to Group 3 in September 2021, you will pledge for Adhiveshan in the Group 2 categories. Anyone in Groups 1-3 ( as of January 2021) will also attend the <b>Bal/Balika Summer Shibir</b>. <br/><b>NOTE</b>: Current 8th graders will pledge for the Kishore/Kishori Adhiveshan and will join the Kishore/Kishori Shibir. <br/> Those currently in Group 0 will pledge for the Group 0 adhiveshan, even if they are graduating and moving to Group 1 in September 2021; those currently in Group 0 will join the <b>Bal/Balika Group 0 Shibir.</b>',
         },
         {
             'question': 'What does Adhiveshan consist of?',
-            'answer': 'Adhiveshan will have three parts, each assessed in different ways and at different times. The '
-                      'first part is the Beginner’s Challenge (Prathmik Mukhpath) which will be assessed at home. The '
-                      'second part is the Adhiveshan Rajipo Challenge (Main Mukhpath), which will be assessed online '
-                      '(in real time - over Zoom). The final part is the Skills Challenge (Talent Competitions), '
-                      'which will be judged through regional online submissions.',
+            'answer': 'Adhiveshan will have three parts, each assessed in different ways and at different times throughout the spring and summer months: <br/> <ul> <li>The first part is the <b>Beginner’s Challenge</b> (traditionally known as Prathmik Mukhpath) which will be assessed at home.</li><li>The second part is the <b>Adhiveshan Rajipo Challenge</b> (traditionally known as Advance Mukhpath), which will be assessed online (in real time - over a video conferencing platform).</li><li>The final part is the <b>Skills Challenge</b> (traditionally known as Talent Competitions), which will be judged through regional online submissions.</li></ul>',
         },
     ],
     'Beginner’s Challenge (Prathmik Mukhpath)': [
