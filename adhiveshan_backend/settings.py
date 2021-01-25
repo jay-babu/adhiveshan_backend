47b58cb7a8511c205592896841d4dbc4054c86b3
Kirtan Patel <kirtan96@gmail.com>
2021-01-24 22:15:47 -0800

    fix

--- adhiveshan_backend/settings.py
+++ adhiveshan_backend/settings.py
@@ -120,11 +120,11 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "https://bkadhiveshan.na.baps.org",
     "capacitor://localhost",
-    "http://localhost"
+    "http://localhost",
 ]
 
 if getenv('ENV_PROD', 'False') == 'False':
-    CORS_ALLOWED_ORIGINS = ["http://localhost:8000", "http://localhost:8100", "*"]
+    CORS_ALLOWED_ORIGINS = ["http://localhost:8000", "http://localhost:8100",]
 
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.Argon2PasswordHasher',
