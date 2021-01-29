737acf1f62d4316ce854fff5c409f7c9322cf73d
Darshan <darshanvkalola@gmail.com>
2021-01-29 13:26:41 -0500

    Add title search for mukhpath items

--- main/admin.py
+++ main/admin.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 
 from . import models
 
+
+class MukhpathItemAdmin(admin.ModelAdmin):
+    search_fields = ('title',)
+
+
 admin.site.register(models.User)
 admin.site.register(models.Pledge)
 admin.site.register(models.PledgedModule)
 admin.site.register(models.Module)
 admin.site.register(models.ModuleInstance)
-admin.site.register(models.MukhpathItem)
+admin.site.register([models.MukhpathItem, MukhpathItemAdmin])
 admin.site.register(models.MukhpathItemInstance)
