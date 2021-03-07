e247776e4097aa8fe5ba1321123d7f6517ab8209
Darshan <darshanvkalola@gmail.com>
2021-03-07 14:42:55 -0500

    Add search to module instance

--- main/admin.py
+++ main/admin.py
@@ -8,9 +8,13 @@ class MukhpathItemAdmin(admin.ModelAdmin):
     search_fields = ('title',)
 
 
+@admin.register(models.ModuleInstance)
+class ModuleInstanceAdmin(admin.ModelAdmin):
+    search_fields = ('module',)
+
+
 admin.site.register(models.User)
 admin.site.register(models.Pledge)
 admin.site.register(models.PledgedModule)
 admin.site.register(models.Module)
-admin.site.register(models.ModuleInstance)
 admin.site.register(models.MukhpathItemInstance)
