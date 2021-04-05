6e4452cea391dfa7d6524328acfe6d63fa0fe3da
Kirtan Patel <kirtan96@gmail.com>
2021-04-05 15:03:41 -0700

    Added a missing colon

--- main/constants.py
+++ main/constants.py
@@ -5459,7 +5459,7 @@ KM_STORIES = [
 BM_ANNOUNCEMENTS = [
     {
         'timestamp': '5 Apr, 2021',
-        'text': 'For both Rajipo and Gurubhakti challenge\n\n1. Everything may be memorized in any language: Sanskrit, Gujarati, or English. \n\n 2. While encouraged, you do not need to memorize the translations/meanings for any mukhpath. \n\n 3. You will not be tested on references for any mukhpath.'
+        'text': 'For both Rajipo and Gurubhakti challenge:\n\n1. Everything may be memorized in any language: Sanskrit, Gujarati, or English. \n\n 2. While encouraged, you do not need to memorize the translations/meanings for any mukhpath. \n\n 3. You will not be tested on references for any mukhpath.'
     },
     {
         'timestamp': '7 Feb, 2021',
@@ -5470,7 +5470,7 @@ BM_ANNOUNCEMENTS = [
 KM_ANNOUNCEMENTS = [
     {
         'timestamp': '5 Apr, 2021',
-        'text': 'For both Rajipo and Gurubhakti challenge\n\n1. Everything may be memorized in any language: Sanskrit, Gujarati, or English. \n\n 2. While encouraged, you do not need to memorize the translations/meanings for any mukhpath. \n\n 3. You will not be tested on references for any mukhpath.'
+        'text': 'For both Rajipo and Gurubhakti challenge:\n\n1. Everything may be memorized in any language: Sanskrit, Gujarati, or English. \n\n 2. While encouraged, you do not need to memorize the translations/meanings for any mukhpath. \n\n 3. You will not be tested on references for any mukhpath.'
     },
     {
         'timestamp': '7 Feb, 2021',
