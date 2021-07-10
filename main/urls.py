d78f072b0c8ad53e599a1b22a0784967c032de36
Darshan <darshanvkalola@gmail.com>
2021-07-10 00:43:56 -0400

    Add divyabhav and sahan modules

--- main/urls.py
+++ main/urls.py
@@ -43,6 +43,6 @@ urlpatterns = [
     path('', include(router.urls)),
 
     # Dev use only
-    # path('upload_content/', views.UploadContentView.as_view(), name='upload_content'),
+    path('upload_content/', views.UploadContentView.as_view(), name='upload_content'),
     # path('add_modules/', views.AddModulesToDB.as_view(), name='add_modules'),
 ]
